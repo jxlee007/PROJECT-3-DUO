a5148b358ab7d69bc1672027ce5a476690967dbb
jagmohan <jimmyfalco007@gmail.com>
2024-02-13 01:09:26 +0530

    done

---
+++ script.js
@@ -0,0 +1,284 @@
+locoScroll = () => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
+locoScroll();
+
+// -----------------------------------
+
+dot = () => {
+    var main = document.querySelector('#main');
+    var vid = document.querySelector('.page1 video')
+    var dot = document.querySelector('#cursor');
+    
+    document.addEventListener('mousemove', function (dets) {
+        // using gsap for smooth flow
+        gsap.to(dot, {
+            x: dets.x, // Adding 10 to dets.x
+            y: dets.y,
+            opacity: 1,
+        })
+    })
+    
+    main.addEventListener('mouseleave', function (dets) {
+        gsap.to(dot, {
+            scale: 1,
+            opacity: 0,
+        })
+    })
+}
+dot();
+
+// ----------------------------------------------------------
+
+gsap.from(".page1 h1,.page1 h2", {
+    y: 30,
+    rotate: 10,
+    opacity: 0,
+    delay: 0.3,
+    duration: 1
+})
+
+an = () => {
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            // trigger: '.page1 h1',
+            trigger: '.page1 h1',
+            scroller: '#main',
+            // markers: true,
+            scrub: 3,
+            start: 'top 30%',
+            end: 'top 0',
+        }
+    })
+    
+    tl.to('.page1 h1', {
+        x: -100,
+        duration: 1,
+    }, 'anim') // add extra var = anim for execute both at same time
+    tl.to('.page1 h2', {
+        x: 100,
+        duration: 1,
+    }, 'anim')
+    tl.to('.page1 video', {
+        width: '90%',
+        delay: .1,
+    },)
+    
+    var tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.page1 h1',
+            scroller: '#main',
+            // markers: true,
+            scrub: 3,
+            start: 'top -120%',
+            end: 'top -130%',
+        }
+    })
+    tl2.to('#main', {
+        backgroundColor: '#fff',
+    })
+    
+    var tl3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.page1 h1',
+            scroller: '#main',
+            // markers: true,
+            scrub: 3,
+            start: 'top -280%',
+            end: 'top -300%',
+        }
+    })
+    tl3.to('#main', {
+        backgroundColor: '#0f0d0d',
+        color: 'white',
+    })
+        
+}
+an();
+
+// -------------------------------------------------------
+
+vid = () => {
+    var p1 = document.querySelector('page1')
+    var vid = document.querySelector('.page1 video')
+    var dot = document.querySelector('#cursor');
+    
+    // const transVal = dot.style.transition;
+    
+    
+    vid.addEventListener('mouseenter', function (dets) {
+        dot.innerHTML = "sound on";
+        dot.style.width = '7%';
+        dot.style.display = 'flex';
+        dot.style.alignItems = 'center';
+        dot.style.justifyContent = 'center';
+        dot.style.fontSize = '12px';
+        dot.style.padding = ' 0px 15px';
+        dot.style.borderRadius = '50px';
+        dot.style.color = '#000';
+        // dot.style.transition = '0.3s ease-in'
+    });
+    
+    vid.addEventListener('click', function (dets) {
+        if (vid.muted) {
+            vid.muted = false;
+            dot.innerHTML = "sound off";
+        } else {
+            vid.muted = true;
+            dot.innerHTML = "";
+        }
+    });
+    
+    vid.addEventListener('mouseleave', function (dets) {
+        dot.innerHTML = ""; // Clear the content of the dot element
+        dot.style.width = '0'; // Reset the width of the dot element
+        dot.style.width = '30px';
+        dot.style.display = 'block';
+        dot.style.fontSize = '0';
+        dot.style.padding = ' 0';
+        dot.style.borderRadius = '50px';
+        // dot.style.transition = 'background-image ease 0.5s'
+        gsap.to(dot, {
+            opacity: 1,
+    
+        })
+    });
+    
+    }
+    vid();
+
+// -----------------------------------------------------
+
+loop = ()=>{
+    // Select all the h4 tags in the nav tag
+let navH4s = document.querySelectorAll('nav h4');
+
+// Loop through each h4 tag
+navH4s.forEach(function(navH4) {
+    // Add mouseover event listener
+    navH4.addEventListener('mouseover', function() {
+        // Get the h4 content
+        let h4Content = this.innerHTML;
+
+        // Select all the h1 tags in the div with the elem class and nav id
+        let elemH1s = document.querySelectorAll('#nav .marquee h1');
+
+        // Loop through each h1 tag
+        elemH1s.forEach(function(elemH1) {
+            // Set the h1 content
+            elemH1.innerHTML = h4Content;
+            elemH1.style.fontSize = '10vw';
+            elemH1.style.fontWeight = '100';
+            elemH1.style.zIndex = '7';
+
+
+        });
+    });
+});
+
+var nav = document.querySelector('#nav')
+var navi = document.querySelector('nav')
+var h4 = document.querySelectorAll('nav h4')
+h4.forEach(function (elem) {
+    elem.addEventListener('mousemove', function (dets) {
+        nav.style.display = 'flex';
+        nav.style.opacity = '1';
+    })
+    navi.addEventListener('mouseleave', function (dets) {
+        nav.style.display = 'none';
+        nav.style.opacity = '0';
+    })
+
+});
+
+
+}
+loop();
+
+// ------------------------------------------
+
+box = ()=>{
+
+    // var p4 = document.querySelector('.page4')
+
+    document.querySelectorAll('.box').forEach(function(elem){
+
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.querySelector("img"), {
+                opacity: 1,
+                ease: Power3,
+                x: dets.x ,
+                y: dets.y ,
+            });
+        });
+
+        elem.addEventListener("mouseleave", function(dets) {
+            gsap.to(elem.querySelector("img"), {
+                opacity: 0,
+                ease: Power3,
+                duration: 0.5,
+            });
+        });
+    
+
+    });
+}
+box();
+
+// ------------------------------------
+// var dot = document.querySelector('#cursor')
+
+// var boxes = document.querySelectorAll('.box');
+//     boxes.forEach(function (elem) {
+//         elem.addEventListener('mouseenter', function (dets) {
+//             var att = elem.getAttribute('data-image')
+//             dot.style.width = '30vw';
+//             dot.style.height = '45vh';
+//             dot.style.mixBlendMode = 'normal';
+//             dot.style.borderRadius = '0';
+//             dot.style.backgroundImage = `url(${att})`;
+        
+
+
+//         })
+//         elem.addEventListener('mouseleave', function (dets) {
+//             dot.style.width = '30px';
+//             dot.style.height = '30px';
+//             dot.style.borderRadius = '50%';
+//             dot.style.backgroundImage = 'none';
+//             dot.style.mixBlendMode = 'difference';
+//         })
+
+//     })
+
+
+
+
